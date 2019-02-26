864cacacbf4a51d834f844f845c0c4dcb5bffa7a
yulyharuka <yulyharuka@gmail.com>
2019-02-26 16:39:45 +0700

    add connection to postgres

--- router/connection.js
+++ router/connection.js
@@ -1,1 +1,29 @@
-// Diisi connect ke postgres
+// Diisi menyesuaikan settingan pgsql masing2
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+const client = new Client({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+})
+client.connect()
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
