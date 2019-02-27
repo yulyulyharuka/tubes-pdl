a1173d20483f36d54b33706f1da43a362993bdd8
Fadhriga Bestari <fadhriga.bestari@gmail.com>
2019-02-27 19:23:05 +0700

    Add .env for database logging

--- router/connection.js
+++ router/connection.js
@@ -1,6 +1,12 @@
 // Diisi menyesuaikan settingan pgsql masing2
-var pg = require('pg')
-var conString = "postgres://postgres:ghosina11@localhost:5432/asdf";
+require('dotenv').config();
+
+var pg = require('pg');
+let password = process.env.DB_PASSWORD;
+let port = process.env.DB_PORT;
+let db_name = process.env.DB_NAME;
+
+var conString = "postgres://postgres:" + password + "@localhost:" + port + "/" + db_name + "";
 
 var client = new pg.Client(conString);
 client.connect()
