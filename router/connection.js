a5ae2dd3677d3e1609f0b0efc89a5baaddca1c83
BobMcSaggy <ajienaufal0804@gmail.com>
2019-02-27 18:12:28 +0700

    Add query page layouting

--- router/connection.js
+++ router/connection.js
@@ -1,6 +1,6 @@
 // Diisi menyesuaikan settingan pgsql masing2
 var pg = require('pg')
-var conString = "postgres://postgres:password@localhost:5432/tubes";
+var conString = "postgres://postgres:ghosina11@localhost:5432/asdf";
 
 var client = new pg.Client(conString);
 client.connect()
